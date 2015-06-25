23f1bbc619a3c1936f9ff3d49c432441bbb4924c
Dadsaster <olliebradley76.com>
2015-06-25 14:21:47 -0700

    Created simple math quiz

---
+++ questions.py
@@ -0,0 +1,17 @@
+__author__ = 'Oliver'
+
+class Question(object):
+    answer = None
+    text = None
+
+
+class Add(Question):
+    def __init__(self, num1, num2):
+        self.text = '{} + {}'.format(num1, num2)
+        self.answer = num1 + num2
+
+class Multiply(Question):
+    def __init__(self, num1, num2):
+        self.text = '{} x {}'.format(num1, num2)
+        self.answer = num1 * num2
+
